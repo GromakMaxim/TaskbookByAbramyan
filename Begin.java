e71d0fa3c19d4767009c5ab7b4b6e2e14e91d76c
Gromak Maxim <maximgromak@gmail.com>
2020-09-16 16:07:41 +1000

    add begin2

--- Begin.java
+++ Begin.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
+
 class Begin {
 
     public void begin1() {
         Scanner input = new Scanner(System.in);
+        System.out.println("Begin1. Дана сторона квадрата a. Найти его периметр P = 4·a.");
         System.out.println("Введите длину стороны квадрата, чтобы узнать его периметр");
-        int side = input.nextInt();
-        int perimetr = 4 * side;
-        System.out.println(input.nextInt() * 4);
+        System.out.println("Периметр: " + input.nextInt() * 4);
+    }
+
+    public void begin2() {
+        System.out.println("Begin2. Дана сторона квадрата a. Найти его площадь S = a2.");
+        System.out.println("Введите сторону квадрата, чтобы узнать его площадь");
+        Scanner myScan = new Scanner(System.in);
+        System.out.println("Площадь: " + Math.pow(myScan.nextInt(), 2));
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	Begin begin = new Begin();
-	begin.begin1();
+        Begin begin = new Begin();
+        Scanner myScan = new Scanner(System.in);
+        int menuPoint = myScan.nextInt();
+        switch (menuPoint) {
+            case 1:
+                begin.begin1();
+                break;
+            case 2:
+                begin.begin2();
+                break;
+        }
+
+
     }
 }
